d18951b5f6a45a53f3ebdaad96a7cbffff162c55
marsha-1 <marshajovanc@student.unimelb.edu.au>
2026-05-18 23:26:18 +1000

    added search function and pop up ads, and added all the products info card

--- products.js
+++ products.js
@@ -150,6 +150,118 @@ const products = {
       ItemNumber: '#29231',
     }
   },
+  'tezcatlipoca-natural-boulder-opal-ring': {
+    name: 'Tezcatlipoca Natural Boulder Opal Ring',
+    price: 'AU$4,351.65',
+    image: 'rings/Tezcatlipoca Natural Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'bright-green-black-natural-crystal-opal-ring': {
+    name: 'Bright Green Black Natural Crystal Opal Ring',
+    price: 'AU$2,066.65',
+    image: 'rings/Bright Green Black  Natural Crystal Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'blue-queensland-boulder-opal-ring': {
+    name: 'Blue Queensland Boulder Opal Ring',
+    price: 'AU$1,671.65',
+    image: 'rings/Blue Queensland Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'signature-queensland-boulder-opal-ring': {
+    name: 'Signature Queensland Boulder Opal Ring',
+    price: 'AU$666.65',
+    image: 'rings/Signature Queensland Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'ocean-breeze-queensland-boulder-opal-ring': {
+    name: 'Ocean Breeze Queensland Boulder Opal Ring',
+    price: 'AU$499.15',
+    image: 'rings/Ocean Breeze Queensland Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'midnight-tide-boulder-opal-ring': {
+    name: 'Midnight Tide Boulder Opal Ring',
+    price: 'AU$666.65',
+    image: 'rings/Midnight Tide Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'aurora-earth-natural-boulder-opal-ring': {
+    name: 'Aurora Earth Natural Boulder Opal Ring',
+    price: 'AU$398.65',
+    image: 'rings/Aurora Earth Natural Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'stone-sea-natural-opal-ring': {
+    name: 'Stone & Sea Natural Opal Ring',
+    price: 'AU$499.15',
+    image: 'rings/Stone & Sea  Natural Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'window-to-the-outback-boulder-opal-ring': {
+    name: 'Window to the Outback Boulder Opal Ring',
+    price: 'AU$1,671.65',
+    image: 'rings/Window to the Outback Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'blue-brilliance-black-opal-ring': {
+    name: 'Blue Brilliance Black Opal Ring',
+    price: 'AU$2,006.65',
+    image: 'rings/Blue Brilliance Black Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'prismatic-flash-boulder-opal-ring': {
+    name: 'Prismatic Flash Boulder Opal Ring',
+    price: 'AU$2,341.65',
+    image: 'rings/Prismatic Flash  Boulder Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'clover-circle-black-opal-ring': {
+    name: 'Clover Circle Black Opal Ring',
+    price: 'AU$2,509.15',
+    image: 'rings/Clover Circle Black Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'fire-on-the-plains-crystal-opal-ring': {
+    name: 'Fire on the Plains Crystal Opal Ring',
+    price: 'AU$2,006.65',
+    image: 'rings/Fire on the Plains Crystal Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'white-and-blue-crystal-opal-ring': {
+    name: 'White and Blue Crystal Opal Ring',
+    price: 'AU$666.65',
+    image: 'rings/White and Blue Crystal Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'fire-on-the-sky-crystal-opal-ring': {
+    name: 'Fire on the Sky Crystal Opal Ring',
+    price: 'AU$1,671.65',
+    image: 'rings/Fire on the Sky Crystal Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
+  'multi-stone-crystal-opal-ring': {
+    name: 'Multi Stone Crystal Opal Ring',
+    price: 'AU$1,671.65',
+    image: 'rings/Multi Stone Crystal  Opal Ring.jpg',
+    category: 'rings',
+    categoryName: 'RINGS',
+  },
 
 // earrings
   'natural-crystal-opal-stud-earrings': {
@@ -189,7 +301,308 @@ const products = {
       ItemNumber: '#29733',
     }
   },
-  
+  'solid-white-opal-stud-earrings': {
+    name: 'Solid White Opal Stud Earrings',
+    price: 'AU$586.25',
+    image: 'earrings/Solid White Opal Stud Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'statement-crystal-opal-stud-earrings': {
+    name: 'Statement Crystal Opal Stud Earrings',
+    price: 'AU$398.65',
+    image: 'earrings/Statement Crystal Opal Stud Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'lagoon-view-boulder-opal-earrings': {
+    name: 'Lagoon View Boulder Opal Earrings',
+    price: 'AU$2,174.15',
+    image: 'earrings/Lagoon View  Boulder Opal Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'colour-bars-boulder-opal-drop-earrings': {
+    name: 'Colour Bars Boulder Opal Drop Earrings',
+    price: 'AU$1,336.65',
+    image: 'earrings/Colour Bars Boulder Opal Drop Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'purple-crystal-teardrop-opal-earrings': {
+    name: 'Purple Crystal Teardrop Opal Earrings',
+    price: 'AU$666.65',
+    image: 'earrings/Purple Crystal Teardrop Opal Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'white-opal-drop-earrings': {
+    name: 'White Opal Drop Earrings',
+    price: 'AU$499.15',
+    image: 'earrings/White Opal  Drop Opal Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'coastal-eclipse-boulder-opal-earrings': {
+    name: 'Coastal Eclipse Boulder Opal Earrings',
+    price: 'AU$1,671.65',
+    image: 'earrings/Coastal Eclipse Boulder Opal Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'ganymede-and-callisto-solid-opal-earrings': {
+    name: 'Ganymede and Callisto Solid Opal Earrings',
+    price: 'AU$2,174.15',
+    image: 'earrings/Ganymede and Callisto  Solid Opal Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'green-stars-large-crystal-opal-earrings': {
+    name: 'Green Stars Large Crystal Opal Earrings',
+    price: 'AU$3,011.65',
+    image: 'earrings/Green Stars Large  Crystal Opal Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'blue-and-gold-boulder-opal-drop-earrings': {
+    name: 'Blue and Gold Boulder Opal Drop Earrings',
+    price: 'AU$2,006.65',
+    image: 'earrings/Blue and Gold Boulder  Opal Drop Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'white-statement-opal-drop-earrings': {
+    name: 'White Statement Opal Drop Earrings',
+    price: 'AU$264.65',
+    image: 'earrings/White Statement  Opal Drop Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+  'majestic-crystal-opal-drop-earrings': {
+    name: 'Majestic Crystal Opal Drop Earrings',
+    price: 'AU$264.65',
+    image: 'earrings/Majestic Crystal Opal Drop Earrings.jpg',
+    category: 'earrings',
+    categoryName: 'EARRINGS',
+  },
+
+//bracelets
+'teal-crystal-opal-5-stone-bracelet': {
+  name: 'Teal Crystal Opal 5 Stone Bracelet',
+  price: 'AU$499.15',
+  image: 'bracelets/Teal Crystal Opal  5 Stone Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'grand-opal-bracelet': {
+  name: 'Grand Opal Bracelet',
+  price: 'AU$1,169.15',
+  image: 'bracelets/Grand Opal Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'teal-green-light-opal-4-stone-bracelet': {
+  name: 'Teal & Green Light Opal 4 Stone Bracelet',
+  price: 'AU$499.15',
+  image: 'bracelets/Teal & Green Light Opal  4 Stone Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'teal-green-light-opal-5-stone-bracelet': {
+  name: 'Teal & Green Light Opal 5 Stone Bracelet',
+  price: 'AU$499.15',
+  image: 'bracelets/Teal & Green Light Opal 5 Stone Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'light-oval-large-stone-opal-bracelet': {
+  name: 'Light Oval Large Stone Opal Bracelet',
+  price: 'AU$1,336.65',
+  image: 'bracelets/Light Oval Large  Stone Opal Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'tear-drop-large-stone-crystal-opal-bracelet': {
+  name: 'Tear-drop Large Stone Crystal Opal Bracelet',
+  price: 'AU$1,671.65',
+  image: 'bracelets/Tear-drop Large Stone Crystal Opal Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'light-opal-large-stone-bracelet': {
+  name: 'Light Opal Large Stone Bracelet',
+  price: 'AU$1,336.65',
+  image: 'bracelets/Light Opal Large  Stone Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'white-opal-large-stone-bracelet': {
+  name: 'White Opal Large Stone Bracelet',
+  price: 'AU$1,169.15',
+  image: 'bracelets/White Opal Large  Stone Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'adjustable-crystal-opal-bracelet': {
+  name: 'Adjustable Crystal Opal Bracelet',
+  price: 'AU$499.15',
+  image: 'bracelets/Adjustable Crystal  Opal Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'light-pearl-opal-bracelet': {
+  name: 'Light Pearl Opal Bracelet',
+  price: 'AU$499.15',
+  image: 'bracelets/Light Pearl Opal Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'2-stacks-crystal-opal-bracelet': {
+  name: '2 Stacks Crystal Opal Bracelet',
+  price: 'AU$586.25',
+  image: 'bracelets/2 Stacks Crystal Opal Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'white-crystal-opal-cuff-bracelet': {
+  name: 'White Crystal Opal Cuff Bracelet',
+  price: 'AU$666.65',
+  image: 'bracelets/White Crystal Opal  Cuff Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'solid-queensland-boulder-opal-bracelet': {
+  name: 'Solid Queensland Boulder Opal Bracelet',
+  price: 'AU$1,504.15',
+  image: 'bracelets/Solid Queensland Boulder Opal Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'green-crystal-opal-10-stone-bracelet': {
+  name: 'Green Crystal Opal 10 Stone Bracelet',
+  price: 'AU$398.65',
+  image: 'bracelets/Green Crystal Opal 10 Stone Bracelet.jpg',
+  category: 'bracelets',
+  categoryName: 'BRACELETS',
+},
+
+'classic-opal-necklace': {
+  name: 'Classic Opal Necklace',
+  price: 'AU$398.65',
+  image: 'necklaces/Classic Opal Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'brilliant-opal-necklace': {
+  name: 'Brilliant Opal Necklace',
+  price: 'AU$1,169.15',
+  image: 'necklaces/Brilliant Opal Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'statement-opal-necklace': {
+  name: 'Statement Opal Necklace',
+  price: 'AU$398.65',
+  image: 'necklaces/Statement Opal Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'crystal-opal-2-hearts-necklace': {
+  name: 'Crystal Opal 2 Hearts Necklace',
+  price: 'AU$398.65',
+  image: 'necklaces/Crystal Opal 2 Hearts Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'crystal-opal-flower-necklace': {
+  name: 'Crystal Opal Flower Necklace',
+  price: 'AU$398.65',
+  image: 'necklaces/Crystal Opal  Flower Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'crystal-opal-heart-necklace': {
+  name: 'Crystal Opal Heart Necklace',
+  price: 'AU$398.65',
+  image: 'necklaces/Crystal Opal Heart Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'crystal-opal-necklace': {
+  name: 'Crystal Opal Necklace',
+  price: 'AU$1,169.15',
+  image: 'necklaces/Classic Opal Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'crystal-opal-squiggle-necklace': {
+  name: 'Crystal Opal Squiggle Necklace',
+  price: 'AU$398.65',
+  image: 'necklaces/Crystal Opal  Squiggle Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'crystal-opal-wing-necklace': {
+  name: 'Crystal Opal Wing Necklace',
+  price: 'AU$398.65',
+  image: 'necklaces/Crystal Opal Wing Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'crystal-opal-hook-necklace': {
+  name: 'Crystal Opal Hook Necklace',
+  price: 'AU$499.15',
+  image: 'necklaces/Crystal Opal  Hook Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'lightning-mcboulder-boulder-opal-necklace': {
+  name: 'Lightning McBoulder Boulder Opal Necklace',
+  price: 'AU$2,006.65',
+  image: 'necklaces/Lightning McBoulder Boulder Opal Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'river-fire-boulder-opal-necklace': {
+  name: 'River Fire Boulder Opal Necklace',
+  price: 'AU$2,006.65',
+  image: 'necklaces/River Fire Boulder Opal Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
+'soul-of-the-sea-solid-black-opal-necklace': {
+  name: 'Soul of the Sea Solid Black Opal Necklace',
+  price: 'AU$16,713.15',
+  image: 'necklaces/Soul of the Sea Solid Black Opal Necklace.jpg',
+  category: 'necklaces',
+  categoryName: 'NECKLACES',
+},
+
 };
 
 const params = new URLSearchParams(window.location.search);
@@ -227,25 +640,28 @@ const thumbnails = document.querySelectorAll('.thumbnail');
       }
     });
   }
+}
 
 const otherProducts = Object.entries(products).filter(([key]) => key !== id);
 const shuffled = otherProducts.sort(() => Math.random() - 0.5);
 const exploring = shuffled.slice(0, 4);
 
+const cards = document.querySelectorAll('.explore-card');
+const images = document.querySelectorAll('.explore-img');
+
 exploring.forEach(([key, p], index) => {
   const num = index + 1;
-  const imgEl = document.querySelectorAll('.explore-img')[index];
+
+  if (cards[index]) cards[index].href = `products.html?id=${key}`;
+
+  if (images[index]) {
+    images[index].src = p.image;
+    images[index].alt = p.name;
+  }
+
   const nameEl = document.getElementById(`explore-name-${num}`);
   const priceEl = document.getElementById(`explore-price-${num}`);
 
-  if (imgEl) imgEl.src = p.image;
   if (nameEl) nameEl.textContent = p.name;
   if (priceEl) priceEl.textContent = p.price;
-
-  if (imgEl) {
-    imgEl.parentElement.parentElement.parentElement.addEventListener('click', () => {
-      window.location.href = `product.html?id=${key}`;
-    });
-  }
 });
-}
--- script.js
+++ script.js
@@ -72,3 +72,80 @@ filterBtns.forEach(btn => {
     });
   });
 });
+
+
+const searchInput = document.getElementById('search-input');
+
+if (searchInput) {
+  searchInput.addEventListener('input', () => {
+    const query = searchInput.value.toLowerCase();
+    const resultsDiv = document.getElementById('search-results');
+    resultsDiv.innerHTML = '';
+
+    if (query.length < 2) return;
+
+    const matches = Object.entries(products).filter(([key, p]) =>
+      p.name.toLowerCase().includes(query)
+    );
+
+    if (matches.length === 0) {
+      resultsDiv.innerHTML = '<p>No results found.</p>';
+      return;
+    }
+
+      matches.slice(0, 4).forEach(([key, p]) => {        
+      resultsDiv.innerHTML += `
+        <div class="search-result-item" onclick="window.location.href='products.html?id=${key}'">
+          <img src="${p.image}" alt="${p.name}">
+          <div>
+            <h2>${p.name}</h2>
+            <p>${p.price}</p>
+          </div>
+        </div>
+      `;
+    });
+  });
+}
+
+const searchResultsGrid = document.getElementById('search-results-grid');
+
+if (searchResultsGrid) {
+  const params = new URLSearchParams(window.location.search);
+  const query = params.get('q');
+
+  if (query) {
+    document.getElementById('search-title').textContent = `RESULTS FOR "${query}"`;
+
+    const matches = Object.entries(products).filter(([key, p]) =>
+      p.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    if (matches.length === 0) {
+      searchResultsGrid.innerHTML = '<p style="padding: 2rem;">No results found.</p>';
+    } else {
+      matches.forEach(([key, p]) => {        
+        searchResultsGrid.innerHTML += `
+          <a href="products.html?id=${key}">
+            <div class="category-img-wrap">
+              <img src="${p.image}" alt="${p.name}">
+              <p class="explore">QUICK VIEW</p>
+            </div>
+            <h2 style="margin-top: 5px;">${p.name}</h2>
+            <p>${p.price}</p>
+          </a>
+        `;
+      });
+    }
+  }
+}
+
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("close-popup");
+
+setTimeout(() => {
+    popup.style.display = "flex";
+}, 2000);
+
+closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+});
